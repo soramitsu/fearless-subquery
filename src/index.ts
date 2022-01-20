31e5a85d2756359fe39103854f8b030b7ca5c4fe
bgodlin <bogodline@gmail.com>
2022-01-20 18:05:01 +0300

    Unnecessary indexing logic removed

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
 //Exports all handler functions
 export * from './mappings/HistoryElements'
-export * from './mappings/Rewards'
 export * from './mappings/Transfers'
-export * from './mappings/NewEra'
-export * from './mappings/StakeChanged'
